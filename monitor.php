761c2474a25303d624782dcb2e3b3197eeb07eae
Alex Funk <lexotron314@gmail.com>
2016-12-28 16:42:19 -0600

    Use ELBV2.
    Modified the software to work with ElasticLoadBalancerV2.  Upgraded to
    aws-php-sdk 3.

--- monitor.php
+++ monitor.php
@@ -21,100 +21,131 @@ require 'config.php';
 require 'utilities.php';
 require 'vendor/autoload.php';
 
-use Aws\Common\Aws;
-
-$aws = Aws::factory(array(
-    'key' => $AWS_CONF['key'],
-    'secret' => $AWS_CONF['secret'],
-    'region' => $AWS_CONF['region']
+$aws = new Aws\Sdk(array(
+    'credentials' => [
+        'key' => $AWS_CONF['key'],
+        'secret' => $AWS_CONF['secret']
+    ],
+    'region' => $AWS_CONF['region'],
+    'version' => 'latest'
 ));
 
-$elbClient = $aws->get('ElasticLoadBalancing');
-
-$balancers = $elbClient->describeLoadBalancers(array(
-    'LoadBalancerNames' => array($AWS_CONF['load_balancer_name'])
-));
-
-if (empty($balancers)) {
-    trigger_error('Failed to obtain description of load balancer.', E_USER_ERROR);
-}
-if (empty($balancers['LoadBalancerDescriptions'][0]['Instances'])) {
-    trigger_error('No EC2 instances found.', E_USER_ERROR);
-}
-
-$elbEc2instances = $balancers['LoadBalancerDescriptions'][0]['Instances'];
-
-$slavesIDs = array();
-foreach ($elbEc2instances as $instance) {
-    $instanceID = $instance['InstanceId'];
-    if ($instanceID != $AWS_CONF['master_ec2_instance_id']) {
-        $slavesIDs[] = $instanceID;
-    }
-}
-
-if (empty($slavesIDs)) {
-    trigger_error('No slave instances found.', E_USER_ERROR);
-}
-
-if (!hasSlavesChanged($slavesIDs, $APP_CONF['data_dir'] . 'slaves')) {
-    echo "No changes in slaves.\n";
-    keepLsyncdAlive($APP_CONF);
-    exit();
-}
-
-echo "There are changes in slaves.\n";
-
-$toRun = null;
-if (isset($APP_CONF['remote_script']['enabled']) && $APP_CONF['remote_script']['enabled']) {
-    $scriptConfig = $APP_CONF['remote_script'];
-    echo "Remote script execution is enabled.\n";
-
-    if (!is_readable($scriptConfig['local_path'])) {
-        trigger_error('Remote script is not present or readable at ' . $scriptConfig['local_path']);
-    }
-
-    $toRun = $slavesIDs;
-    if ($APP_CONF['remote_script']['run_script_on_all_slaves'] === false) {
-        $oldSlaves = getSavedSlaves($APP_CONF['data_dir'] . 'slaves');
-        $toRun = array_diff($slavesIDs, $oldSlaves);
-    }
-
-}
-
-saveSlaves($slavesIDs, $APP_CONF['data_dir'] . 'slaves');
-
-$ec2Client = $aws->get('Ec2');
-
-$ec2Instances = $ec2Client->describeInstances(array('InstanceIds' => $slavesIDs));
-
-if (empty($ec2Instances)) {
-    trigger_error('Unable to obtain description of slave EC2 instances.', E_USER_ERROR);
-}
-
-$slaves = array();
-
-foreach ($ec2Instances['Reservations'] as $reservation) {
-    $instances = $reservation['Instances'];
-
-    foreach ($instances as $instance) {
-        $slaves[] = array(
-            'instance_id' => $instance['InstanceId'],
-            'private_ip_address' => $instance['PrivateIpAddress']
-        );
-    }
-}
+$ec2Client = $aws->createEC2();
 
 /**
- * Run remote script on slaves
+ * Test if I'm the master.  If not, lsync config is easy.
  */
-if (!empty($toRun)) {
-    foreach ($slaves as $slave) {
-        if (in_array($slave['instance_id'], $toRun)) {
-            echo "Executing remote script at instance $slave[instance_id] ($slave[private_ip_address]) ...\n";
-            $command = 'cat ' . $APP_CONF['remote_script']['local_path'] . ' | ssh -i ' . $LSYNCD_CONF['ssh_private_key'] . ' ' . $LSYNCD_CONF['ssh_user'] . '@' . $slave['private_ip_address'];
-            passthru($command);
+
+$instance_id = file_get_contents("http://instance-data/latest/meta-data/instance-id");
+
+if ($instance_id == $AWS_CONF['master_ec2_instance_id']) {
+
+    $elbClient = $aws->createElasticLoadBalancingV2();
+    
+    $results = $elbClient->describeTargetHealth([
+        'TargetGroupArn' => $AWS_CONF['target_group_arn']
+    ]);
+    
+    $balancers = array();
+    $targetHealthDescriptions = $results['TargetHealthDescriptions'];
+    
+    foreach ($targetHealthDescriptions as $r) {
+        $balancers[] = $r['Target']['Id'];
+    }
+    
+    
+    if (empty($balancers)) {
+        trigger_error('No EC2 instances found.', E_USER_ERROR);
+    }
+    
+    $slavesIDs = array();
+    foreach ($balancers as $instance) {
+        if ($instance != $AWS_CONF['master_ec2_instance_id']) {
+            $slavesIDs[] = $instance;
         }
     }
+    
+    if (empty($slavesIDs)) {
+        trigger_error('No slave instances found.', E_USER_ERROR);
+    }
+    
+    if (!hasSlavesChanged($slavesIDs, $APP_CONF['data_dir'] . 'slaves')) {
+        echo "No changes in slaves.\n";
+        keepLsyncdAlive($APP_CONF);
+        exit();
+    }
+    
+    echo "There are changes in slaves.\n";
+    
+    $toRun = null;
+    if (isset($APP_CONF['remote_script']['enabled']) && $APP_CONF['remote_script']['enabled']) {
+        $scriptConfig = $APP_CONF['remote_script'];
+        echo "Remote script execution is enabled.\n";
+    
+        if (!is_readable($scriptConfig['local_path'])) {
+            trigger_error('Remote script is not present or readable at ' . $scriptConfig['local_path']);
+        }
+    
+        $toRun = $slavesIDs;
+        if ($APP_CONF['remote_script']['run_script_on_all_slaves'] === false) {
+            $oldSlaves = getSavedSlaves($APP_CONF['data_dir'] . 'slaves');
+            $toRun = array_diff($slavesIDs, $oldSlaves);
+        }
+    
+    }
+    
+    saveSlaves($slavesIDs, $APP_CONF['data_dir'] . 'slaves');
+    
+    
+    $ec2Instances = $ec2Client->describeInstances(array('InstanceIds' => array($AWS_CONF['master_ec2_instance_id'])));
+//     $ec2Instances = $ec2Client->describeInstances(array('InstanceIds' => $slavesIDs));
+    
+    if (empty($ec2Instances)) {
+        trigger_error('Unable to obtain description of slave EC2 instances.', E_USER_ERROR);
+    }
+    
+    var_dump($ec2Instances);
+    die();
+    
+    $slaves = array();
+    
+    foreach ($ec2Instances['Reservations'] as $reservation) {
+        $instances = $reservation['Instances'];
+    
+        foreach ($instances as $instance) {
+            $slaves[] = array(
+                'instance_id' => $instance['InstanceId'],
+                'private_ip_address' => $instance['PrivateIpAddress']
+            );
+        }
+    }
+    
+    /**
+     * Run remote script on slaves
+     */
+    if (!empty($toRun)) {
+        foreach ($slaves as $slave) {
+            if (in_array($slave['instance_id'], $toRun)) {
+                echo "Executing remote script at instance $slave[instance_id] ($slave[private_ip_address]) ...\n";
+                $command = 'cat ' . $APP_CONF['remote_script']['local_path'] . ' | ssh -i ' . $LSYNCD_CONF['ssh_private_key'] . ' ' . $LSYNCD_CONF['ssh_user'] . '@' . $slave['private_ip_address'];
+                passthru($command);
+            }
+        }
+    }
+
+} else {
+    $slaves = array();
+   
+    $ec2Instances = $ec2Client->describeInstances(array('InstanceIds' => array($AWS_CONF['master_ec2_instance_id'])));
+        
+    $slaves[] = array(
+        'instance_id' => $AWS_CONF['master_ec2_instance_id'],
+        'private_ip_address' => $ec2Instances['Reservations'][0]['Instances'][0]['PrivateIpAddress']      
+    );
+    
+    $LSYNCD_CONF['source'] .= '/wp-content';
+    $LSYNCD_CONF['target_dir'] .= '/wp-content';
+    
 }
 
 /**
